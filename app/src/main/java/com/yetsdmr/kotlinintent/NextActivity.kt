f740a4c152afc0a7a387d7b138bd090c86c2799d
EMRE TAŞDEMİR <yetsdmr@gmail.com>
2020-08-19 16:17:50 +0300

    Initial commit

---
+++ app/src/main/java/com/yetsdmr/kotlinintent/NextActivity.kt
@@ -0,0 +1,20 @@
+package com.yetsdmr.kotlinintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_next.*
+
+class NextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+
+        //getIntent --> java
+        val intent = intent
+        val username = intent.getStringExtra("username")
+        val name = intent.getStringExtra("name")
+
+        userNameTextNextActivity.text = "Username: " + username
+        nameTextNextActivity.text = "Name: " + name
+    }
+}
